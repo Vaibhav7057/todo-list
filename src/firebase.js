ac996c9c544a8d09486083e3f8a33464e03c8271
Vaibhav <vaibhavgedamvg.vg@gmail.com>
2023-10-24 08:52:49 +0530

    my first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Bg_eixfnkedpxjUPP0JltVhGFjX-JlE",
+  authDomain: "todo-app-cp-1edd3.firebaseapp.com",
+  projectId: "todo-app-cp-1edd3",
+  storageBucket: "todo-app-cp-1edd3.appspot.com",
+  messagingSenderId: "1028230006596",
+  appId: "1:1028230006596:web:8795dc28e77e546bbdb911",
+  measurementId: "G-8TL02FDW5C",
+};
