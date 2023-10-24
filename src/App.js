ef83a077188d26788bf03670fc0c929c17a38bc8
Vaibhav <vaibhavgedamvg.vg@gmail.com>
2023-10-24 18:29:13 +0530

    authentication added with auth0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import Profile from "./Components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="app">
       <h1>Hello world!</h1>
+      {!isAuthenticated ? (
+        <div>
+          <h4>Login</h4>
+          <Login />
+        </div>
+      ) : (
+        <div>
+          <Logout />
+          <Profile />
+        </div>
+      )}
     </div>
   );
 }
